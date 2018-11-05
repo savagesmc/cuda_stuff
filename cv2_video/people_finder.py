2f902dc2ba4908ac52a64c3076928e623d2e5e78
Clark, Steve <steve@clarkhome.org>
2018-11-05 14:54:15 -0600

    Updates to people finding and scene change detection.

--- cv2_video/people_finder.py
+++ cv2_video/people_finder.py
@@ -1,8 +1,10 @@
-import cv2, argparse, json, sys
+import cv2, argparse, json, sys, csv, datetime
 import subprocess as sp
 import numpy as np
 import scipy, scipy.signal
+import scipy.cluster as sc
 import matplotlib.pyplot as plt
+
 
 ''' Give a json from ffprobe command line
 
@@ -121,25 +123,106 @@ class Finder:
             last = i
       return finds
 
+def doKmeans(time, values, debug=False):
+   centroids,_ = sc.vq.kmeans(values, 2)
+   idx,_ = sc.vq.vq(values, centroids)
+   idx = ((idx - 0.5) * -2. + 1.) / 2.
+   if debug:
+      print(len(value), len(idx))
+      for i in range(len(idx)):
+         debug("{} : {} : {}".format(i, value[i], idx[i]))
+      plt.plot(time, idx)
+      plt.show()
+   return idx
+
 if __name__ == "__main__":
    parser = argparse.ArgumentParser()
    parser.add_argument("-n", "--filename", help="name of video file to process", default="video.mp4")
    parser.add_argument("-i", "--interval", help="frame skip interval", type=int, default=30)
    parser.add_argument("-c", "--csvoutput", help="write analysis to csv", default="")
+   parser.add_argument("--csvinput", help="read analysis from csv", default="")
    parser.add_argument("-o", "--output", help="write bounding box to video avi file", default="")
    parser.add_argument("-Fs", help="sample frequency (hz)", type=float, default = 30)
    parser.add_argument("-Fc", help="low pass filter center frequency (hz)", type=float, default = 0.01)
-   parser.add_argument("-N", help="low pass filter number of taps", type=float, default = 256)
+   parser.add_argument("-N", help="low pass filter number of taps", type=int, default = 256)
    parser.add_argument("-d", "--debug", help="debug", action='store_true')
+   parser.add_argument("--printOptions", action='store_true')
    args = parser.parse_args()
-   finder = Finder(args.filename, args.output)
-   stats = finder.analyze(interval=args.interval, debug=args.debug)
-   cv2.destroyAllWindows()
-   times = np.array([s[0]/finder.fps for s in stats])
-   num = doFilter(np.array([len(s[1]) for s in stats]), args.N, args.Fs, args.Fc)
+
+   if args.printOptions:
+      for arg in vars(args):
+         print("{:20} = {}".format(arg, getattr(args, arg)))
+      sys.exit(0)
+
+   if not args.csvinput:
+      finder = Finder(args.filename, args.output)
+      stats = finder.analyze(interval=args.interval, debug=args.debug)
+      cv2.destroyAllWindows()
+      times = np.array([s[0]/finder.fps for s in stats])
+      num = doFilter(np.array([len(s[1]) for s in stats]), args.N, args.Fs, args.Fc)
+      classified = doKmeans(times, num, debug=args.debug)
+
+   else:
+      times = []
+      num = []
+      classified = []
+
+      with open(args.csvinput) as csv_file:
+         csv_reader = csv.reader(csv_file, delimiter=',')
+         lineCount = 0
+         for row in csv_reader:
+            times.append(float(row[0]))
+            num.append(float(row[1]))
+            classified.append(float(row[2]))
+         print("data input")
+
+      times = np.array(times)
+      num = np.array(num)
+      classified = np.array(classified)
+      for i in range(len(classified)):
+         print("{} : {} : {} : {}".format(i, times[i], num[i], classified[i]))
+
    if args.csvoutput:
       with open(args.csvoutput, "w") as ofile:
          for i in range(len(times)):
-            ofile.write("{},{}\n".format(times[i], num[i]))
+            ofile.write("{},{},{}\n".format(times[i], num[i], classified[i]))
+
+   starts = []
+   ends = []
+   dur = []
+   prev=0
+   for i in range(len(times)):
+      cur = classified[i]
+      if cur != prev:
+         if cur == 1:
+            starts.append(times[i])
+         else:
+            ends.append(times[i])
+            if len(starts) != len(ends):
+              starts.append(times[i])
+         prev = cur
+
+   for i in range(min(len(starts), len(ends))):
+      dur.append(ends[i] - starts[i])
+
+   starts = np.array(starts)
+   ends = np.array(ends)
+   dur = np.array(dur)
+
+   def getTime(x):
+      hour = int(x/3600)
+      x -= 3600*hour
+      minute = int(x/60)
+      x -= 60*minute
+      second = int(x)
+      x -= second
+      return datetime.time(hour=hour, minute=minute, second=second, microsecond=int(x*1e6))
+
+   sermonIdx = np.argmax(dur)
+   startTime = getTime(starts[sermonIdx]-args.N/2)
+   endTime = getTime(ends[sermonIdx]-args.N/2)
+   print("Sermon Start:  {}  {}  {}".format(startTime, endTime, getTime(dur[sermonIdx])))
+
    plt.plot(times, num)
+   plt.plot(times, classified*max(num))
    plt.show()
--- cv2_video/plot.py
+++ cv2_video/plot.py
@@ -1,16 +1,29 @@
 import csv
+import numpy as np
 import matplotlib.pyplot as plt
+import scipy.cluster as sc
 
 time = []
 value = []
-
 
 with open('output.csv') as csv_file:
    csv_reader = csv.reader(csv_file, delimiter=',')
    lineCount = 0
    for row in csv_reader:
-      time.append(row[0])
-      value.append(row[1])
+      time.append(float(row[0]))
+      value.append(float(row[1]))
 
-plt.plot(time, value)
+print("data input")
+
+time = np.reshape(time, (len(time), 1))
+value = np.reshape(value, (len(value), 1))
+centroids,_  = sc.vq.kmeans(value, 2)
+idx,_ = sc.vq.vq(value, centroids)
+
+print(len(value), len(idx))
+
+for i in range(len(idx)):
+   print("{} : {} : {}".format(i, value[i], idx[i]))
+
+plt.plot(time, idx)
 plt.show()
